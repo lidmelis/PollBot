0b1bcab671c0e5f280127f67f21894d4e95ef6cd
lidmelis <lidaalgyun@gmail.com>
2025-03-03 20:02:01 +0300

    Set mvp models

--- app/bot/commands.py
+++ app/bot/commands.py
@@ -1,10 +1,11 @@
 from aiogram import Router
-from app.bot.handlers import start, help #, forms, stats
+from app.bot.handlers import start, help, forms #, stats
 
 def register_handlers(dp):
     main_router = Router()
 
     main_router.include_router(start.router)
     main_router.include_router(help.router)
+    main_router.include_router(forms.router)
 
     dp.include_router(main_router)
