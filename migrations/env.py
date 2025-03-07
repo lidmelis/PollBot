777ae4adce05f5e7b69eb4e00718ca8925718d4d
lidmelis <lidaalgyun@gmail.com>
2025-03-07 23:01:02 +0300

    tidied up

--- migrations/env.py
+++ migrations/env.py
@@ -12,7 +12,6 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.db import Base
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
